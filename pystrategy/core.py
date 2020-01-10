bef351853181991b314bedba39a787e7fe10fefa
James Williams <jwilliams1@resurgent.com>
2020-01-10 13:30:45 -0500

    refactor, error formatting, xor functionality

--- pystrategy/core.py
+++ pystrategy/core.py
@@ -1,17 +1,15 @@
 import operator
 import json
-import operators as custom_op
-
-# TODO add XOR conjuction
+from .operators import between, not_between, not_contains, in_, not_in, re_contains
 
 class Evaluation():
     """The simplest logical unit in an evaluation.
 
     Attributes:
-        func_ (function): comparision operator function
+        func_ (function): comparison operator function
         field_ (str): name of field where field_value resides in evaluation payload.
         value_ (str/float): value to compare to field_value
-        op_str_ (str): string representation of comparision operator
+        op_str_ (str): string representation of comparison operator
     """
 
     def __init__(self, field, value, operator_str):
@@ -24,14 +22,16 @@ class Evaluation():
             "ge": operator.ge,
             "gt": operator.gt,
             "contains": operator.contains,
-            "not contains": custom_op.not_contains,
-            "in": custom_op.in_,
-            "not in": custom_op.not_in,
-            "regex match": custom_op.re_contains
+            "not contains": not_contains,
+            "in": in_,
+            "not in": not_in,
+            "regex match": re_contains,
+            "between": between,
+            "not between": not_between
         }
 
         if operator_str not in operators.keys():
-            raise ValueError(f'Operator must be a valid value. {operator_str} is not valid.')
+            raise ValueError(f"Operator must be a valid value. '{operator_str}' is not valid.")
 
         self.func_ = operators[operator_str]
         self.field_ = field
@@ -52,13 +52,13 @@ class Evaluation():
         # find the value to compare in the payload dict
         field_value_ = payload.get(self.field_)
         if not field_value_:
-            raise ValueError(f'Required field {self.field_} not in payload.')
+            raise ValueError(f"Required field '{self.field_}' not in payload.")
         
         if verbose:
             tabs = "\t" * level
             print(tabs + f"Evaluating {field_value_} {self.op_str_} {self.value_}")
         
-        # run the comparision operation based on the initialzed operator
+        # run the comparison operation based on the initialzed operator
         result = self.func_(field_value_, self.value_)
         if verbose: print(tabs + f"Evaluation Result: {result}")
         
@@ -66,18 +66,18 @@ class Evaluation():
 
 class Composite():
     """A logical composite made of children evaluations joined by logical
-    OR or AND (conjunction). Children evaluations can be Composites themselves,
+    OR or AND or XOR (conjunction). Children evaluations can be Composites themselves,
     or Evaluations.
 
     Attributes:
-        conjuction_ (str): Logical AND or OR
+        conjuction_ (str): Logical AND or OR or XOR
         children_ (list): list of logical children
     """
 
     def __init__(self, children, conjuction='AND'):
         
-        if conjuction not in ['AND', 'OR']:
-            raise ValueError('Conjuction must be a valid value.')
+        if conjuction not in ['AND', 'OR', 'XOR']:
+            raise ValueError(f"Conjuction must be a valid value. '{conjuction}' is not valid.")
 
         if not isinstance(children, list):
             raise ValueError('Children must be a list, empty or otherwise.')
@@ -114,7 +114,7 @@ class Composite():
 
         # if children are joined by OR, evaluate every child until all children
         # are evaluated or until a True breaks the loop (only need 1 True for OR)
-        else:
+        elif self.conjuction_ == 'OR':
             result = False
             i = 0
             while result == False and (i < len(self.children_)):
@@ -126,6 +126,25 @@ class Composite():
                 
                 result = self.children_[i].evaluate(payload, level + 1, verbose=verbose)
                 i += 1
+
+        # XOR evaluation - 1 and only 1 can be True. Have to iterate over all children unless the number of trues becomes greater than 1
+        else:
+            i = 0
+            true_count = 0
+            while true_count < 2 and (i < len(self.children_)):
+                if verbose:
+                    tabs = "\t" * level
+                    if i > 0: print("\n" + tabs + f"{self.conjuction_} \n")
+                    print(tabs + f"Evaluating Composite: {i + 1}, Level: {level + 1}")
+                    
+                    # += a boolean is equivalent to += 1 for T and += 0 for False
+                    true_count += self.children_[i].evaluate(payload, level + 1, verbose=verbose)
+                    i += 1
+
+            if true_count == 1:
+                result = True
+            else:
+                result = False
 
         if verbose: 
             tabs = "\t" * level
@@ -197,5 +216,8 @@ class JSONEvaluationEngine():
 
         return self.composite_.evaluate(payload, verbose=verbose)
 
+    #def pretty_print(self):
+    #    pass
 
-
+    #def __repr__(self):
+    #    self.pretty_print()
