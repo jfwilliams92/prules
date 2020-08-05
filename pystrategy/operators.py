98bdf51df931028daedab6994663b216f9f3e09b
James Williams <jwilliams1@resurgent.com>
2020-08-05 10:30:53 -0400

    add doc strings

--- pystrategy/operators.py
+++ pystrategy/operators.py
@@ -1,34 +1,36 @@
 import re
 
 def between(a, b):
-
-    # since reading from JSON, values are either string, float, or list of
+    """Evaluates a between b[0] and b[1]"""
+    # since reading from JSON, values are either string, float, or list of values
     if not isinstance(b, list):
         raise ValueError('other value must be a list of length 2')
     
     return b[0] <= a <= b[1]
 
 def not_between(a, b):
+    """Evaluates a not between b[0] and b[1]"""
     if not isinstance(b, list):
         raise ValueError('other value must be a list of length 2')
 
     result = b[0] <= a <= b[1]
-
     return False if result else True
 
 def in_(a, b):
     return a in b
 
 def not_in(a, b):
+    """Evalutes a not in b""""
     result = False if a in b else True
     return result
 
 def not_contains(a, b):
+    """Evaluates a does not contain b"""
     result = False if b in a else True
     return result
 
 def re_contains(a, b):
-    """Return True a regex search with pattern b yields a match in a
+    """Return True if a regex search with pattern b yields a match in a
 
     Args:
         a (str): Pattern to search
