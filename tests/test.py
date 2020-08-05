902001e271a5fee83b6a2f2a7977da41b7de25f7
James Williams <jwilliams1@resurgent.com>
2020-08-05 14:04:53 -0400

    setting up unit tests

---
+++ tests/test.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestEvaluation(unittest.TestCase):
+    # TODO test that every operator behaves as expected
+    pass
+
+class TestComposite(unittest.TestCase):
+    # TODO test that every logical gate/conjuction behaves as expected
+    pass
+
+class TestEngine(unittest.TestCase):
+    # TODO test that the Json Eval engine works as expected
+    pass 
